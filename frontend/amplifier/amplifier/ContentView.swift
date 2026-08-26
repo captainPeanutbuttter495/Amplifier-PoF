7de107286b3b077aff69b9919d2065ed0b5ee442
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2026-08-26 13:47:08 -0700

    Init frontend

---
+++ frontend/amplifier/amplifier/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  amplifier
+//
+//  Created by Jeremiah Cabahug on 8/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        test
+    }
+}
+
+#Preview {
+    ContentView()
+}
